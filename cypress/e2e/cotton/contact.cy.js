d93ac6d31c10501a1d4ff3613b09b44b3711b680
irfan91catalyze <irfan@catalyzecommunications.com>
2023-05-25 10:24:44 +0700

    contact

---
+++ cypress/e2e/cotton/contact.cy.js
@@ -0,0 +1,47 @@
+import rgbHex from 'rgb-hex';
+describe('Contact', () => { 
+  describe('Memastikan halaman Contact berhasil ditampilkan dan berfungsi dengan baik sesuai desain yang ada', () => { 
+    beforeEach(() => {
+      cy.viewport(1920, 1280)
+      cy.visit('/contact')
+      cy.general();
+    })
+
+    it('Akses halaman Contact', () => { 
+        cy.get('header').should("be.visible")
+        cy.get('.banner').should("be.visible")
+        cy.get('.banner .content .container > h1').contains('Contact')
+        cy.get('.content-area .list > img').eq(0).should('have.attr', 'src').should('include','pesticide.jpg')
+        cy.get('.content-area .list .content > p').eq(0).contains('Integrated pest management and agroecology, pesticide regulation and policy, farmer capacity building, health of farmers and workers, and biodiversity.')
+        cy.get('.content-area .list .content .cp > a > img').eq(0).should('have.attr', 'src').should('include','ic_email.svg')
+        cy.get('.content-area .list .content .cp > a > span').eq(0).contains('rajan@pan-uk.org')
+        cy.get('.content-area .list .content .social-media .name').eq(0).contains('Join the discussion at:')
+        cy.get('.content-area .list .content .social-media .link-area > a > img').eq(0).should('have.attr', 'src').should('include','ic_linkedin.svg')
+        cy.get("section.banner").eq(0).should('have.css', 'background-color').and('be.colored', '#fff');
+
+        cy.get('.content-area .list > img').eq(1).should('have.attr', 'src').should('include','solidaridad.jpg')
+        cy.get('.content-area .list .content > p').eq(3).contains('For questions about the cotton paper, as well as centring smallholder farmers, living income for farmers, decent work and living wage for workers, farmer training in good agricultural practice, fair value distribution, and farmer participation, contact Solidaridad at:')
+        cy.get('.content-area .list .content .cp > a > img').eq(0).should('have.attr', 'src').should('include','ic_email.svg')
+        cy.get('.content-area .list .content .cp > a > span').eq(1).contains('tamar.hoek@solidaridadnetwork.org')
+        cy.get('.content-area .list .content .social-media .name').eq(1).contains('Join the discussion at:')
+        cy.get('.content-area .list .content .social-media .link-area > a > img').eq(3).should('have.attr', 'src').should('include','ic_linkedin.svg')
+        cy.get("section.banner").eq(0).should('have.css', 'background-color').and('be.colored', '#fff');
+
+        cy.scrollTo('bottom')
+        cy.wait(5000)
+        cy.get('footer').should("be.visible")
+    });
+
+    it.only('Hover dan klik pada email', () => {
+      cy.scrollTo('bottom')
+      cy.wait(5000)
+      cy.get('.content-area .list .content .cp > a > span').eq(0).realHover();
+      cy.get('.content-area .list .content .cp > a > span').eq(0).should('have.css', 'color').and('be.colored', '#319044');
+    });
+
+    it('Hover dan klik pada salah satu join social media (LinkedIn, Facebook, dan/atau Twitter)', () => {
+      
+    });
+
+  })
+})
